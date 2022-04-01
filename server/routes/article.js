0c1d47d93c05a9a9082edb4e05c0fb21bc2e3814
Bharat Pratap <bp171020@gmail.com>
2022-04-01 16:18:28 +0530

    routes done

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,1 +1,42 @@
-const articlecontroller = require('./../models/Article')
+const articlecontroller = require('./../controllers/article.ctrl')
+const multipart = require('connect-multiparty');
+const multipartWare = multipart()
+
+module.exports = (router) => {
+    /**
+     * get all articles
+     */
+    router
+        .route('/articles')
+        .get(articlecontroller.getAll)
+
+    /**
+     * add an article
+     */
+    router
+        .route('/article')
+        .post(multipartWare,articlecontroller.addArticle)
+    
+    /**
+     * clap on article
+     */
+    router
+        .route('/article/clap')
+        .post(articlecontroller.clapArticle)
+
+    /**
+     * comment on article
+     */
+    router
+        .route('/article/comment')
+        .post(articlecontroller.commentArticle)
+
+    /**
+     * get a particular article to view
+     */
+    router
+        .route('/article/:id')
+        .post(articlecontroller.getArticle)
+
+
+}
